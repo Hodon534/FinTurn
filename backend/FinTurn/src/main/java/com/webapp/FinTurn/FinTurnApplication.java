104f21a17cbb4046b09d8d71eb892a2835cc45f7
Konrad <hodon534@gmail.com>
2024-01-16 22:10:11 +0100

    initial commit

---
+++ backend/FinTurn/src/main/java/com/webapp/FinTurn/FinTurnApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.FinTurn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTurnApplication.class, args);
+	}
+
+}
---
+++ backend/FinTurn/src/test/java/com/webapp/FinTurn/FinTurnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.FinTurn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
