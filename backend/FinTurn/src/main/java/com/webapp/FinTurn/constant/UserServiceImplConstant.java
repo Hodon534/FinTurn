2e47047168d16d0a74002fb271f7220a257d7006
Konrad <hodon534@gmail.com>
2024-01-17 23:32:01 +0100

    Text and numeric values used in user-related service

---
+++ backend/FinTurn/src/main/java/com/webapp/FinTurn/constant/UserServiceImplConstant.java
@@ -0,0 +1,11 @@
+package com.webapp.FinTurn.constant;
+
+public class UserServiceImplConstant {
+    public static final String USERNAME_ALREADY_EXIST = "Username already exist";
+    public static final String EMAIL_ALREADY_EXIST = "Email already exist";
+    public static final String NO_USER_FOUND_BY_USERNAME = "No user found by username ";
+    public static final String RETURNING_FOUND_USER_BY_USERNAME = "Returning found user by username: ";
+    public static final String EMPTY = "";
+    public static final String NO_USER_FOUND_BY_EMAIL = "No user found for email: ";
+    public static final String NOT_AN_IMAGE_FILE = " is not an image file. Please upload an image.";
+}
