f759df303be3ff79c968ceb620cd7fa34178a680
Konrad <hodon534@gmail.com>
2024-01-17 10:23:11 +0100

    generated constant with authorities that users with certain role will have

---
+++ backend/FinTurn/src/main/java/com/webapp/FinTurn/constant/Authorities.java
@@ -0,0 +1,10 @@
+package com.webapp.FinTurn.constant;
+
+public class Authorities {
+    public static final String[] USER_AUTHORITIES = { "user:read" };
+    public static final String[] MANAGER_AUTHORITIES = { "user:read", "user:update" };
+    public static final String[] ADMIN_AUTHORITIES = { "user:read", "user:update", "user:create" };
+    public static final String[] SUPER_ADMIN_AUTHORITIES = { "user:read", "user:update", "user:create", "user:delete" };
+
+
+}
