a8ba63fa271b1abd01c492c6e0f8951d443c046d
Konrad <hodon534@gmail.com>
2024-01-20 12:23:03 +0100

    custom exceptions

---
+++ backend/FinTurn/src/main/java/com/webapp/FinTurn/exception/domain/NotAnImageFileException.java
@@ -0,0 +1,7 @@
+package com.webapp.FinTurn.exception.domain;
+
+public class NotAnImageFileException extends RuntimeException {
+    public NotAnImageFileException(String message) {
+        super(message);
+    }
+}
