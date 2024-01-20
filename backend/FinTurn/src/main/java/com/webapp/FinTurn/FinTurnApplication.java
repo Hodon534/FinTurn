1cd54a0979f95b48c88d806a27f65ac83cdb0bbb
Konrad <hodon534@gmail.com>
2024-01-20 16:07:52 +0100

    dependencies update

--- backend/FinTurn/src/main/java/com/webapp/FinTurn/FinTurnApplication.java
+++ backend/FinTurn/src/main/java/com/webapp/FinTurn/FinTurnApplication.java
@@ -1,13 +1,18 @@
 package com.webapp.FinTurn;
 
+import com.webapp.FinTurn.constant.FileConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class FinTurnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinTurnApplication.class, args);
+		new File(FileConstant.USER_FOLDER).mkdirs(); // create folder in User Directory
+
 	}
 
 }
