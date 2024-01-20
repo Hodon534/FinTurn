059c562b0887027494c35400d05ee98fed37a3ab
Konrad <hodon534@gmail.com>
2024-01-20 16:04:37 +0100

    configuration of security

---
+++ backend/FinTurn/src/main/java/com/webapp/FinTurn/configuration/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package com.webapp.FinTurn.configuration;
+
+import com.webapp.FinTurn.constant.SecurityConstant;
+import com.webapp.FinTurn.filter.JwtAccessDeniedHandler;
+import com.webapp.FinTurn.filter.JwtAuthenticationEntryPoint;
+import com.webapp.FinTurn.filter.JwtAuthorizationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(proxyTargetClass = true)
+@Configuration
+public class SecurityConfiguration {
+    private JwtAuthorizationFilter jwtAuthorizationFilter;
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private UserDetailsService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        httpSecuritySessionManagementConfigurer ->
+                                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth -> {
+                            auth.requestMatchers(SecurityConstant.PUBLIC_URLS)
+                                    .permitAll();
+                        }
+                )
+                .exceptionHandling(
+                        exceptionHandlingConfigurer ->
+                                exceptionHandlingConfigurer.accessDeniedHandler(
+                                        jwtAccessDeniedHandler))
+                .httpBasic(
+                        httpBasicConfigurer ->
+                                httpBasicConfigurer.authenticationEntryPoint(
+                                        jwtAuthenticationEntryPoint))
+                .addFilterBefore(
+                        jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+        return http.build();
+    }
+
+/*    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }*/
+}
