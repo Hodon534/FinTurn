8a047859837e9ca45d50dd8ca0599bc87d7f4d1e
Konrad <hodon534@gmail.com>
2024-02-07 22:42:42 +0100

    refactor

---
+++ frontend/finturn-frontend/src/app/pages/admin-panel/settings/settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TabItem, TabsOptions, InstanceOptions, TabsInterface, Tabs } from 'flowbite';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.css'
+})
+export class SettingsComponent {
+
+
+}
