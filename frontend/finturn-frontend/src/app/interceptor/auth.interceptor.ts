2c50a07ad27ced4e6195b34534c045a563565c16
Konrad <hodon534@gmail.com>
2024-02-09 15:35:25 +0100

    authorization interceptor

---
+++ frontend/finturn-frontend/src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,36 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthenticationService } from "../service/authentication.service";
+
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(httpRequest: HttpRequest<any>, httpHandler: HttpHandler): Observable<HttpEvent<any>> {
+    if (httpRequest.url.includes(`${this.authenticationService.host}/`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (httpRequest.url.includes(`${this.authenticationService.host}/home`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (httpRequest.url.includes(`${this.authenticationService.host}/login`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (httpRequest.url.includes(`${this.authenticationService.host}/register`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (httpRequest.url.includes(`${this.authenticationService.host}/reset-password`)) {
+      return httpHandler.handle(httpRequest);
+    } 
+    this.authenticationService.loadToken();
+    const token = this.authenticationService.getToken();
+    const request = httpRequest.clone({ setHeaders: { Authorization: `Bearer ${token}`}});
+    return httpHandler.handle(request);
+
+  }
+  
+}
